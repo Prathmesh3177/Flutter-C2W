9f9d51fcd265643d9b5d47451f510470e09e060b
Prathmesh Bondar <151448851+Prathmesh3177@users.noreply.github.com>
2024-10-19 12:35:06 +0530

    Flutter

---
+++ Flutter/Projects/Complete-To-Do-App/todo_app/lib/todo.dart
@@ -0,0 +1,385 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
+import 'package:todo_app/todo_model_class.dart';
+
+
+class ToDoApp extends StatefulWidget {
+  const ToDoApp({super.key});
+
+  @override
+  State<ToDoApp> createState() => _ToDoAppState();
+}
+
+class _ToDoAppState extends State<ToDoApp> {
+  
+  TextEditingController dateController = TextEditingController();
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+
+  void showBottomSheet(bool doedit, [ToDoModelClass? toDoModelObj]) {
+    showModalBottomSheet(
+      isScrollControlled: true,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(30.0),
+          topRight: Radius.circular(30.0),
+        ),
+      ),
+      isDismissible: true,
+      context: context,
+      builder: (context) {
+        return Padding(
+          padding: EdgeInsets.only(
+            left: 20,
+            right: 20,
+            
+            bottom: MediaQuery.of(context).viewInsets.bottom,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const SizedBox(height: 10),
+              Text(
+                "Create Task",
+                style: GoogleFonts.quicksand(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 22,
+                ),
+              ),
+              const SizedBox(height: 12),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    "Title",
+                    style: GoogleFonts.quicksand(
+                      color: const Color.fromRGBO(0, 139, 148, 1),
+                      fontWeight: FontWeight.w400,
+                      fontSize: 15,
+                    ),
+                  ),
+                  const SizedBox(height: 3),
+                  TextField(
+                    controller: titleController,
+                    decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                        borderSide: const BorderSide(
+                          color: Color.fromRGBO(0, 139, 148, 1),
+                        ),
+                      ),
+                      border: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Colors.purpleAccent),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 12),
+                  Text(
+                    "Description",
+                    style: GoogleFonts.quicksand(
+                      color: const Color.fromRGBO(0, 139, 148, 1),
+                      fontWeight: FontWeight.w400,
+                      fontSize: 15,
+                    ),
+                  ),
+                  const SizedBox(height: 3),
+                  TextField(
+                    controller: descriptionController,
+                    maxLines: 4,
+                    decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                        borderSide: const BorderSide(
+                          color: Color.fromRGBO(0, 139, 148, 1),
+                        ),
+                      ),
+                      border: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Colors.purpleAccent),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 12),
+                  Text(
+                    "Date",
+                    style: GoogleFonts.quicksand(
+                      color: const Color.fromRGBO(0, 139, 148, 1),
+                      fontWeight: FontWeight.w400,
+                      fontSize: 15,
+                    ),
+                  ),
+                  const SizedBox(height: 3),
+                  TextField(
+                    controller: dateController,
+                    readOnly: true,
+                    decoration: InputDecoration(
+                      suffixIcon: const Icon(Icons.date_range_rounded),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                        borderSide: const BorderSide(
+                          color: Color.fromRGBO(0, 139, 148, 1),
+                        ),
+                      ),
+                      border: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Colors.purpleAccent),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    onTap: () async {
+                      DateTime? pickeddate = await showDatePicker(
+                        context: context,
+                        initialDate: DateTime.now(),
+                        firstDate: DateTime(2024),
+                        lastDate: DateTime(2025),
+                      );
+                      String formattedDate = DateFormat.yMMMd().format(pickeddate!);
+                      setState(() {
+                        dateController.text = formattedDate;
+                      });
+                    },
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              Container(
+                height: 50,
+                width: 300,
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(30)),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    backgroundColor: const Color.fromRGBO(0, 139, 148, 1),
+                  ),
+                  onPressed: () {
+                    doedit ? submit(doedit, toDoModelObj) : submit(doedit);
+                    Navigator.of(context).pop();
+                  },
+                  child: Text(
+                    "Submit",
+                    style: GoogleFonts.inter(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w700,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  var listOfColors = [
+    const Color.fromRGBO(250, 232, 232, 1),
+    const Color.fromRGBO(232, 237, 250, 1),
+    const Color.fromRGBO(250, 249, 232, 1),
+    const Color.fromRGBO(250, 232, 250, 1),
+  ];
+
+  List<ToDoModelClass> todoList = [
+    ToDoModelClass(
+      title: "Take notes",
+      description: "Take notes of every app you write",
+      date: "10 July 2023",
+    ),
+    ToDoModelClass(
+      title: "Arrange Meeting",
+      description: "Meet the backend team",
+      date: "10 July 2023",
+    ),
+  ];
+
+  void submit(bool doedit, [ToDoModelClass? toDoModelObj]) {
+    if (titleController.text.trim().isNotEmpty &&
+        descriptionController.text.trim().isNotEmpty &&
+        dateController.text.trim().isNotEmpty) {
+      if (!doedit) {
+        setState(() {
+          todoList.add(
+            ToDoModelClass(
+              title: titleController.text.trim(),
+              description: descriptionController.text.trim(),
+              date: dateController.text.trim(),
+            ),
+          );
+        });
+      } else {
+        setState(() {
+          toDoModelObj!.date = dateController.text.trim();
+          toDoModelObj.title = titleController.text.trim();
+          toDoModelObj.description = descriptionController.text.trim();
+        });
+      }
+    }
+    clearController();
+  }
+
+  
+  void clearController() {
+    titleController.clear();
+    descriptionController.clear();
+    dateController.clear();
+  }
+
+  
+  void removeTasks(ToDoModelClass toDoModelObj) {
+    setState(() {
+      todoList.remove(toDoModelObj);
+    });
+  }
+
+  void editTask(ToDoModelClass toDoModelObj) {
+    
+    titleController.text = toDoModelObj.title;
+    descriptionController.text = toDoModelObj.description;
+    dateController.text = toDoModelObj.date;
+    showBottomSheet(true, toDoModelObj);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    titleController.dispose();
+    dateController.dispose();
+    descriptionController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      appBar: AppBar(
+        backgroundColor: const Color.fromRGBO(2, 167, 177, 1),
+        centerTitle: true,
+        title: Text(
+          "TO-DO APP",
+          style: GoogleFonts.quicksand(
+            fontWeight: FontWeight.w700,
+            fontSize: 25,
+          ),
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: todoList.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 16),
+            child: Container(
+              decoration: BoxDecoration(
+                color: listOfColors[index % listOfColors.length],
+                boxShadow: const [
+                  BoxShadow(
+                    offset: Offset(0, 10),
+                    color: Color.fromRGBO(0, 0, 0, 0.1),
+                    blurRadius: 10,
+                  )
+                ],
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    //1
+                    Row(
+                      children: [
+                        Container(
+                          height: 70,
+                          width: 70,
+                          decoration: const BoxDecoration(
+                            color: Colors.white,
+                            shape: BoxShape.circle,
+                          ),
+                          child: const Center(
+                            child: Icon(
+                              Icons.done_all,
+                              color: Color.fromRGBO(2, 167, 177, 1),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: 20),
+                        Expanded(
+                          child: SizedBox(
+                            height: 70,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                
+                                Text(
+                                  todoList[index].title,
+                                  style: GoogleFonts.quicksand(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w500,
+                                    color: const Color.fromRGBO(2, 167, 177, 1),
+                                  ),
+                                ),
+                                const SizedBox(height: 4),
+                                Text(
+                                  todoList[index].description,
+                                  style: GoogleFonts.quicksand(
+                                    fontWeight: FontWeight.w400,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          todoList[index].date,
+                          style: GoogleFonts.quicksand(
+                            color: const Color.fromRGBO(2, 167, 177, 1),
+                            fontWeight: FontWeight.w500,
+                            fontSize: 15,
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            IconButton(
+                              icon: const Icon(Icons.delete),
+                              onPressed: () {
+                                removeTasks(todoList[index]);
+                              },
+                            ),
+                            IconButton(
+                              icon: const Icon(Icons.edit_note),
+                              onPressed: () {
+                                editTask(todoList[index]);
+                              },
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showBottomSheet(false);
+        },
+        backgroundColor: const Color.fromRGBO(2, 167, 177, 1),
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
